5543a1cba84839add9c1806422a4dc5f3ccb63b7
ldelossa <louis.delos@gmail.com>
2022-01-04 13:36:00 -0500

    fix: re-introduce help buffers
    help buffers were lost in the refactor to separate plugins. this commit
    adds them back
    
    Signed-off-by: ldelossa <louis.delos@gmail.com>

--- lua/litee/filetree/buffer.lua
+++ lua/litee/filetree/buffer.lua
@@ -1,17 +1,17 @@
-local config = require('litee.calltree.config').config
+local config = require('litee.filetree.config').config
 local panel_config = require('litee.lib.config').config["panel"]
 local lib_util_buf = require('litee.lib.util.buffer')
 
 local M = {}
 
 -- _setup_buffer performs an idempotent creation of
--- a calltree buffer.
+-- a filetree buffer.
 function M._setup_buffer(name, buf, tab)
     -- see if we can reuse a buffer that currently exists.
     if buf == nil or not vim.api.nvim_buf_is_valid(buf) then
         buf = vim.api.nvim_create_buf(false, false)
         if buf == 0 then
-            vim.api.nvim_err_writeln("calltree.buffer: buffer create failed")
+            vim.api.nvim_err_writeln("filetree.buffer: buffer create failed")
             return
         end
     else
@@ -21,7 +21,7 @@ function M._setup_buffer(name, buf, tab)
     -- set buf options
     vim.api.nvim_buf_set_name(buf, name .. ":" .. tab)
     vim.api.nvim_buf_set_option(buf, 'bufhidden', 'hide')
-    vim.api.nvim_buf_set_option(buf, 'filetype', 'calltree')
+    vim.api.nvim_buf_set_option(buf, 'filetype', 'filetree')
     vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
     vim.api.nvim_buf_set_option(buf, 'modifiable', false)
     vim.api.nvim_buf_set_option(buf, 'swapfile', false)
@@ -47,6 +47,7 @@ function M._setup_buffer(name, buf, tab)
     vim.api.nvim_buf_set_keymap(buf, "n", "p", ":LTCopyFiletree<CR>", opts)
     vim.api.nvim_buf_set_keymap(buf, "n", "s", ":LTSelectFiletree<CR>", opts)
     vim.api.nvim_buf_set_keymap(buf, "n", "S", ":LTDeSelectFiletree<CR>", opts)
+    vim.api.nvim_buf_set_keymap(buf, "n", "?", ":lua require('litee.filetree').help(true)<CR>", opts)
 	if config.map_resize_keys then
            lib_util_buf.map_resize_keys(panel_config.orientation, buf, opts)
     end
